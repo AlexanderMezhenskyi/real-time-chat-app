860c6a6fcae9e5c8cc69c105ac28dfeb6d97858e
Oleksandr Mezhenskyi <alexmezhenskyi@gmail.com>
2025-06-06 00:57:55 +0200

    feat: add alias

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,12 @@ export default defineConfig({
     open: true,
   },
 
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+
   test: {
     root: import.meta.dirname,
     name: packageJson.name,
